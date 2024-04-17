abcbcb4fa203c90c23e8e26b74afd95ffa6aa190
Ramya95732 <160013562+Ramya95732@users.noreply.github.com>
2024-04-17 16:33:45 +0530

    Update index.js

--- src/components/AboutJob/index.js
+++ src/components/AboutJob/index.js
@@ -31,7 +31,162 @@ class AboutJob extends Component {
     const {match} = props
     const {params} = match
     const {id} = params
-    console.log(match)
+
+    this.setState({apiStatus: apiStatusConstants.inProgress})
+    const jwtToken = Cookies.get('jwt_token')
+    console.log(jwtToken)
+
+    const url = `https://apis.ccbp.in/jobs/${id}`
+    const options = {
+      headers: {
+        Authorization: `Bearer ${jwtToken}`,
+      },
+      method: 'GET',
+    }
+    const response = await fetch(url, options)
+    const data = await response.json()
+    console.log(data)
+
+    if (response.ok === true) {
+      const updatedJobData = [data.job_details].map(eachItem => ({
+        companyLogoUrl: eachItem.company_logo_url,
+        companyWebsiteUrl: eachItem.company_website_url,
+        employmentType: eachItem.employment_type,
+        jobDescription: eachItem.job_description,
+        lifeAtCompany: {
+          description: eachItem.life_at_company.description,
+          imageUrl: eachItem.life_at_company.image_url,
+        },
+        id: eachItem.id,
+        location: eachItem.location,
+        packagePerAnnum: eachItem.package_per_annum,
+        rating: eachItem.rating,
+        skills: eachItem.skills.map(eachSkl => ({
+          name: eachSkl.name,
+          imageUrl: eachSkl.image_url,
+        })),
+        title: eachItem.title,
+      }))
+
+      const updatedSimilarJobs = data.similar_jobs.map(eachItem => ({
+        companyLogoUrl: eachItem.company_logo_url,
+        employmentType: eachItem.employment_type,
+        id: eachItem.id,
+        jobDescription: eachItem.job_description,
+        location: eachItem.location,
+        rating: eachItem.rating,
+        title: eachItem.title,
+      }))
+
+      this.setState({
+        jobDataDetails: updatedJobData,
+        similarjobDetails: updatedSimilarJobs,
+        apiStatus: apiStatusConstants.success,
+      })
+    } else {
+      this.setState({apiStatus: apiStatusConstants.failure})
+    }
+  }
+
+  renderJOBSuccess = () => {
+    const {jobDataDetails, similarjobDetails} = this.state
+    if (jobDataDetails.length >= 1) {
+      const {
+        companyLogoUrl,
+        companyWebsiteUrl,
+        employmentType,
+        jobDescription,
+        lifeAtCompany,
+        location,
+        packagePerAnnum,
+        rating,
+        skills,
+        title,
+        id,
+      } = jobDataDetails[0]
+      return (
+        <>
+          <div>
+            <img src={companyLogoUrl} alt="job details company logo" />
+            <h1>{title}</h1>
+            <AiFillStar />
+            <p>{rating}</p>
+            <MdLocationOn />
+            <p>{location}</p>
+            <p>{employmentType}</p>
+            <hr />
+            <p>{packagePerAnnum}</p>
+            <h1>Description</h1>
+            <a href={companyWebsiteUrl}>
+              Visit
+              <BiLinkExternal />
+            </a>
+            <p>{jobDescription}</p>
+            <h1>Skills</h1>
+            <ul>
+              {skills.map(each => (
+                <li key={each.name}>
+                  <img src={each.imageUrl} alt={each.name} />
+                  <p>{each.name}</p>
+                </li>
+              ))}
+            </ul>
+            <h1>Life at Company</h1>
+            <p>{lifeAtCompany.description}</p>
+            <img src={lifeAtCompany.imageUrl} alt="life at company" />
+            <h1>Similar Jobs</h1>
+            <ul>
+              {similarjobDetails.map(eachJob => (
+                <SimilarJobs
+                  key={eachJob.id}
+                  similarJobData={eachJob}
+                  employmentType={employmentType}
+                />
+              ))}
+            </ul>
+          </div>
+        </>
+      )
+    }
+    return null
+  }
+
+  onRetryJobDEtails = () => {
+    this.getJobData()
+  }
+
+  renderJobFailureView = () => (
+    <div>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/failure-img.png"
+        alt="failure view"
+      />
+      <h1>Oops! Something Went Wrong</h1>
+      <p>We cannot seem to find the page you are looking for.</p>
+      <button type="button" onClick={this.onRetryJobDEtails}>
+        retry
+      </button>
+    </div>
+  )
+
+  onLoading = () => (
+    <div className="loader-container" data-testid="loader">
+      <Loader type="ThreeDots" color="#ffffff" height="50" width="50" />
+    </div>
+  )
+
+  renderJobDetails = () => {
+    const {apiStatus} = this.state
+    switch (apiStatus) {
+      case apiStatusConstants.success:
+        return this.renderJOBSuccess()
+      case apiStatusConstants.failure:
+        return this.renderJobFailureView()
+      case apiStatusConstants.inProgress:
+        return this.onLoading()
+      default:
+        return null
+    }
   }
 
   render() {
